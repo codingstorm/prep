130d7834a9b86b3bc83063276d161e3cd7a79eec
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-12-13 12:11:50 -0700

    Updated

--- source/prep/infrastructure/Sort.cs
+++ source/prep/infrastructure/Sort.cs
@@ -1,21 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using prep.infrastructure.matching;
 
 namespace prep.infrastructure
 {
-    public class Sort<T>
+  public class Sort<ItemToSort>
+  {
+    public static IComparer<ItemToSort> by_descending<PropertyType>(Accessor<ItemToSort, PropertyType> accessor)
     {
-        public Sort()
-        {
-            
-        }
-
-        public static object by_descending(Func<IComparable<collections.Movie>, IComparable<collections.Movie>> func)
-        {
-            throw new NotImplementedException();
-        }
+      throw new NotImplementedException();
     }
+
+    public static IComparer<ItemToSort> by<PropertyType>(Accessor<ItemToSort, PropertyType> accessor)
+    {
+      throw new NotImplementedException();
+    }
+  }
 }
