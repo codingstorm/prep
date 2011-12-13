1493e200690b84de9548101a1f91072cbc798c53
codingstorm <email.raza@gmail.com>
2011-12-13 20:22:25 +0000

    Pushing new changes

--- source/prep/infrastructure/Sort.cs
+++ source/prep/infrastructure/Sort.cs
@@ -4,16 +4,62 @@ using prep.infrastructure.matching;
 
 namespace prep.infrastructure
 {
-  public class Sort<ItemToSort>
+    using prep.collections;
+
+    public class Sort<ItemToSort>
   {
-    public static IComparer<ItemToSort> by_descending<PropertyType>(Accessor<ItemToSort, PropertyType> accessor)
+    public static IComparer<ItemToSort> by_descending<PropertyType>(Accessor<ItemToSort, PropertyType> accessor) where PropertyType : IComparable
     {
-      throw new NotImplementedException();
+        return new AnonymoustOrderableSortComparer<ItemToSort>(new AnonymousSortItemComparer<ItemToSort, PropertyType>(accessor), SortOrder.Descending);
     }
 
-    public static IComparer<ItemToSort> by<PropertyType>(Accessor<ItemToSort, PropertyType> accessor)
+    public static IComparer<ItemToSort> by<PropertyType>(Accessor<ItemToSort, PropertyType> accessor) where PropertyType : IComparable
     {
-      throw new NotImplementedException();
+        return new AnonymousSortItemComparer<ItemToSort, PropertyType>(accessor);
     }
   }
+
+    public enum SortOrder {
+        Ascending,
+        Descending
+    }
+
+    public class AnonymousSortItemComparer<ItemToSort, PropertyType> : IComparer<ItemToSort>
+    where PropertyType: IComparable{
+
+        private readonly Accessor<ItemToSort, PropertyType> accessor;
+
+        public AnonymousSortItemComparer(Accessor<ItemToSort, PropertyType> accessor)
+        {
+            this.accessor = accessor;
+        }
+
+        public int Compare(ItemToSort x, ItemToSort y)
+        {
+            return accessor(x).CompareTo(accessor(y));
+        }
+    }
+
+
+    public class AnonymoustOrderableSortComparer<ItemToSort> : IComparer<ItemToSort>
+    {
+        private readonly IComparer<ItemToSort> comparer;
+
+        private readonly SortOrder sortOrder;
+
+        public AnonymoustOrderableSortComparer(IComparer<ItemToSort> comparer, SortOrder sortOrder)
+        {
+            this.comparer = comparer;
+            this.sortOrder = sortOrder;
+        }
+
+        public int Compare(ItemToSort x, ItemToSort y)
+        {
+            int result = this.comparer.Compare(x, y);
+
+            if (this.sortOrder == SortOrder.Descending) return result * -1;
+
+            return result;
+        }
+    }
 }
